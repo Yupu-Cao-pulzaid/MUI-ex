1bd78720198f73963e3c2d45ae1e754d718f7bac
Yupu-Cao-pulzaid <81758391+Yupu-Cao-pulzaid@users.noreply.github.com>
2021-07-01 10:20:55 -0700

    quick update

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import Header from "./Header";
 import Content from "./Content";
 
 const App = () => {
-  return (
-    <Grid container direction="column">
-      <Grid item>
-        <Header />
-      </Grid>
-      <Grid item container>
-        <Grid item xs={false} sm={2} />
-        <Grid item xs={12} sm={8}>
-          <Content />
-        </Grid>
-        <Grid item xs={false} sm={2} />
-      </Grid>
-    </Grid>
-  );
+    return ( <
+        Grid container direction = "column" >
+        <
+        Grid item >
+        <
+        Header / >
+        <
+        /Grid> <
+        Grid item container >
+        <
+        Grid item xs = { false }
+        sm = { 2 }
+        /> <
+        Grid item xs = { 12 }
+        sm = { 8 } >
+        <
+        Content / >
+        <
+        /Grid> <
+        Grid item xs = { false }
+        sm = { 2 }
+        /> <
+        /Grid> <
+        /Grid>
+    );
 };
 
 export default App;
